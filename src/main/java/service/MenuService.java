07fc34c4c1c48e1cc64b656917e512db32278d1f
RLuchinsky <luchinsky@list.ru>
2018-05-23 15:09:07 +0300

    menu_service

---
+++ src/main/java/service/MenuService.java
@@ -0,0 +1,22 @@
+package service;
+
+import exception.NotFoundException;
+import model.Menu;
+
+import java.util.List;
+
+/**
+ * Created by RLuchinsky on 23.05.2018.
+ */
+public interface MenuService {
+
+    Menu create( Menu menu);
+
+    void delete(int id) throws NotFoundException;
+
+    Menu get(int id) throws NotFoundException;
+
+    List<Menu> getAll(int restId);
+
+    void update(Menu menu);
+}
---
+++ src/main/java/service/MenuServiceImpl.java
@@ -0,0 +1,49 @@
+package service;
+
+import exception.NotFoundException;
+import model.Menu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import repository.MenuRepository;
+
+import java.util.List;
+
+/**
+ * Created by RLuchinsky on 23.05.2018.
+ */
+
+@Service
+public class MenuServiceImpl implements MenuService {
+
+    private MenuRepository repository;
+
+    @Autowired
+    public MenuServiceImpl(MenuRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Menu create(Menu menu) {
+        return repository.save(menu);
+    }
+
+    @Override
+    public void delete(int id) throws NotFoundException {
+repository.delete(id);
+    }
+
+    @Override
+    public Menu get(int id) throws NotFoundException {
+        return repository.get(id);
+    }
+
+    @Override
+    public List<Menu> getAll(int restId) {
+        return repository.getAll(restId);
+    }
+
+    @Override
+    public void update(Menu menu) {
+repository.save(menu);
+    }
+}
